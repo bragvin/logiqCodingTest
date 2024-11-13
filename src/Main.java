c6d3f7a2517f08616fe546ca42d1b96482501fd7
Tobias <131401239+bragvin@users.noreply.github.com>
2024-11-13 22:15:23 +0100

    reads file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        File file = new File("actiontexts.txt");
         
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                System.out.println(data);
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
