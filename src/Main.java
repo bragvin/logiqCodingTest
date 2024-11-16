805df0c78a049ca2e88910bd56066a24c02ce82c
Tobias <131401239+bragvin@users.noreply.github.com>
2024-11-16 15:00:58 +0100

    finished test
    approx time used:
    4 hrs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.io.File;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -14,7 +15,10 @@ public class Main {
                 if (parts.length == 2) {
                     String action = parts[0];
                     String text = parts[1];
-                    System.out.println("Text: [" + text + "] ran action [" + action + "] with result [" + text + "]");
+                    String result = handleString(action, text);
+                    System.out.println("Text: [" + text + "] ran Action: [" + action + "] with result [" + result + "]"); // Utskrift i henhold til eksemplet i oppgaven
+                } else {
+                    System.out.println("Invalid line: " + data); // Feilhåndtering
                 }
             }
             scanner.close();
@@ -23,4 +27,51 @@ public class Main {
             e.printStackTrace();
         }
     }
+    
+    public static String handleString(String action, String text) {
+        switch (action) {
+            case "PALINDROME": // true hvis ordet er et palindrom, ellers false
+                return String.valueOf(isPalindrome(text)); // "Konverterer" boolean til String
+            case "WHITESPACE": // Fjerner alle mellomrom
+                return removeWhitespace(text);
+            case "REVERSE": // Reverserer teksten
+                return reverseText(text);
+            case "UNIQUE_COUNT": // Teller antall unike karakterer i ordet
+                return String.valueOf(uniqueCount(text)); // "Konverterer" int til String
+            case "CAPITALIZE": // Setter stor forbokstav på ordet
+                return doCapitalize(text);
+            default:
+                return "Couldn't find Action.";
+        }
+    }
+
+    public static boolean isPalindrome(String text) {
+        String reversedText = new StringBuilder(text).reverse().toString();
+        return reversedText.equals(text); // Denne returnerer true eller false basert på om ordet er et palindrom
+    }
+
+    public static String removeWhitespace(String text) {
+        String cleanString = text.replaceAll("\\s+", ""); // \\s+ er regex for mellomrom
+        return cleanString;
+    }
+
+    public static String reverseText(String text) {
+        String reversedString = new StringBuilder(text).reverse().toString();
+        return reversedString;
+    }
+
+    public static int uniqueCount(String text) {
+        HashSet<String> word = new HashSet<>(); // HashSet holder bare på unike verdier
+        for (int i = 0; i < text.length(); i++) {
+            word.add(String.valueOf(text.charAt(i))); // Legger til en bokstav for hver iterasjon, antall iterasjoner er lik antall bokstaver i ordet
+        }
+
+        return word.size();
+    }
+
+    public static String doCapitalize(String text) {
+        String capitalizedText = text.substring(0,1).toUpperCase() + text.substring(1); // Deler opp ordet i to subStrings, for første subString bare er første bokstav.
+        return capitalizedText;
+    }
+
 }
